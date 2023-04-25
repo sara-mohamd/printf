3ecc9454d858eff6da47460c81ac64ea2517a924
sara-mohamd <saraamohamed0038@gmail.com>
2023-04-25 13:57:46 -0700

    s

--- fields.c
+++ fields.c
@@ -10,21 +10,21 @@
  */
 char *get_precision(char *s, params_t *myparams, va_list var)
 {
-        int d = 0;
+int d = 0;
 
-        if (*s != '.')
-                return (s);
-        s++;
-        if (*s == '*')
-        {
-                d = va_arg(var, int);
-                s++;
-        }
-        else
-        {
-                while (_isdigit(*s))
-                        d = d * 10 + (*s++ - '0');
-        }
-        myparams->precision = d;
-        return (s);
+if (*s != '.')
+return (s);
+s++;
+if (*s == '*')
+{
+d = va_arg(var, int);
+s++;
+}
+else
+{
+while (_isdigit(*s))
+d = d * 10 + (*s++ - '0');
+}
+myparams->precision = d;
+return (s);
 }
